e7b5c1e732f2f3c4ad4c7c5b28c251596e8f3bbd
beyzatsdln <beyzakavgacii@gmail.com>
2024-07-10 23:06:57 +0300

    commitlendi

--- src/StreamAPI/Main.java
+++ src/StreamAPI/Main.java
@@ -1,2 +1,29 @@
-package StreamAPI;public class Main {
+package StreamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 5, 9,11, 12, 13, 60);
+          Stream<Integer> num = numbers.stream()
+                  .filter(i-> i < 11)
+                  .sorted();
+
+
+        Numbers.printAndFindLessThanEleven(numbers);
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
